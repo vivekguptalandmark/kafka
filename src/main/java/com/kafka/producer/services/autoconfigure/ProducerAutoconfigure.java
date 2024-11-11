d7f0b5600ff989955b9e7044f3378883fc1fbe7e
Vivek Gupta <vivekgupta.gwl@gmail.com>
2024-11-11 09:35:29 +0530

    producer changes

--- src/main/java/com/kafka/producer/services/autoconfigure/ProducerAutoconfigure.java
+++ src/main/java/com/kafka/producer/services/autoconfigure/ProducerAutoconfigure.java
@@ -10,18 +10,8 @@ import org.springframework.kafka.annotation.EnableKafka;
 @EnableKafka
 public class ProducerAutoconfigure {
 
-
-
     @Bean
     ProducerService producerService(StreamBridge streamBridge) {
         return new ProducerService(streamBridge);
     }
-
-//    @Bean
-//    public Consumer<Message<String>> backpressureInputListener() {
-//        return message -> {
-//            // Process the incoming message
-//            System.out.println("Received message: " + message.getPayload());
-//        };
-//    }
 }
