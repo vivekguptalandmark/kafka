54aaca1a7926d3209ce3382d8a79f1537ef04317
Vivek Gupta <vivekgupta.gwl@gmail.com>
2024-11-25 10:22:44 +0530

    hpa added

--- consumer/src/main/java/com/kafka/consumer/services/ConsumerService.java
+++ consumer/src/main/java/com/kafka/consumer/services/ConsumerService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 
@@ -14,9 +16,13 @@ import org.springframework.messaging.support.MessageBuilder;
 public class ConsumerService {
 
     public void processMessage(Message<String> msg) {
+
         try {
             Thread.sleep(2000);
-            log.info("Received message in listener: " + msg.getPayload());
+                log.info("Received message in listener: " + msg.getPayload());
+            Acknowledgment acknowledgment =
+                    msg.getHeaders().get(KafkaHeaders.ACKNOWLEDGMENT, Acknowledgment.class);
+            acknowledgment.acknowledge();
         } catch (InterruptedException e) {
            log.error(e.getMessage());
         }
